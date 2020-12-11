6c7c5bad8d3d21a315be41c3273b981049bc78c7
Juraj Noge <juraj.noge@cleverfarm.cz>
2020-12-11 13:10:58 +0100

    Day 24 - reformat

--- src/main/kotlin/day24.kt
+++ src/main/kotlin/day24.kt
@@ -16,11 +16,12 @@ class Bugs : AdventOfCodeTask {
 
     override fun run(part2: Boolean): Any {
         val originalLevel = readInputLines("24.txt")
-            .flatMapIndexed { y, row -> row.mapIndexed { x, c -> Coordinate(x, y) to c }  }
+            .flatMapIndexed { y, row -> row.mapIndexed { x, c -> Coordinate(x, y) to c } }
             .toMap().toMutableMap()
         emptyLevel = originalLevel.mapValues { '.' }.toMutableMap()
 
-        levels = if (part2) (-(rounds/2)..rounds/2).map{ it to emptyLevel.toMutableMap() }.toMap().toMutableMap() else mutableMapOf()
+        levels = if (part2) (-(rounds / 2)..rounds / 2).map { it to emptyLevel.toMutableMap() }.toMap()
+            .toMutableMap() else mutableMapOf()
         levels[0] = originalLevel
 
         return if (part2) {
@@ -31,15 +32,13 @@ class Bugs : AdventOfCodeTask {
         } else {
             val seen = mutableSetOf<String>()
             var repr = ""
-            while(!seen.contains(repr)) {
+            while (!seen.contains(repr)) {
                 seen.add(repr)
                 turn(recursive = false)
                 repr = levels[0]!!.toStringRepresentation(true)
             }
             calculateBiodiversity()
         }
-
-
     }
 
     private fun turn(recursive: Boolean) {
@@ -48,7 +47,9 @@ class Bugs : AdventOfCodeTask {
             val updated = map.toMutableMap()
             map.forEach { (coordinate, c) ->
                 val adjacent = getAdjacent(coordinate, level, recursive)
-                val count = adjacent.count { (adj, lvl) -> levels.getOrDefault(lvl, emptyLevel.toMutableMap()).getOrDefault(adj, '.') == '#' }
+                val count = adjacent.count { (adj, lvl) ->
+                    levels.getOrDefault(lvl, emptyLevel.toMutableMap()).getOrDefault(adj, '.') == '#'
+                }
                 if (c == '#' && count != 1) {
                     updated[coordinate] = '.'
                 }
@@ -73,8 +74,18 @@ class Bugs : AdventOfCodeTask {
                     (it == center) -> when {
                         (coordinate.x == center.left().x) -> (0..maxY).map { y -> Coordinate(0, y) to level + 1 }
                         (coordinate.x == center.right().x) -> (0..maxY).map { y -> Coordinate(maxX, y) to level + 1 }
-                        (coordinate.y == center.up(offset = true).y) -> (0..maxX).map { x -> Coordinate(x, 0) to level + 1 }
-                        (coordinate.y == center.down(offset = true).y) -> (0..maxX).map { x -> Coordinate(x, maxY) to level + 1 }
+                        (coordinate.y == center.up(offset = true).y) -> (0..maxX).map { x ->
+                            Coordinate(
+                                x,
+                                0
+                            ) to level + 1
+                        }
+                        (coordinate.y == center.down(offset = true).y) -> (0..maxX).map { x ->
+                            Coordinate(
+                                x,
+                                maxY
+                            ) to level + 1
+                        }
                         else -> throw IllegalStateException("${coordinate.x} ${coordinate.y}")
                     }
                     else -> listOf(it to level)
@@ -90,8 +101,10 @@ class Bugs : AdventOfCodeTask {
     }
 
     private fun calculateBiodiversity(): Int {
-        return levels.values.sumBy { it.filterValues { c -> c == '#' }
-            .keys.sumBy { coord -> 2.0.pow(coord.y * 5 + coord.x).toInt() } }
+        return levels.values.sumBy {
+            it.filterValues { c -> c == '#' }
+                .keys.sumBy { coord -> 2.0.pow(coord.y * 5 + coord.x).toInt() }
+        }
     }
 }
 
